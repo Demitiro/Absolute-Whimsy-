c3e1e54545ab5238c12d5378b274ed6272d6ac06
Demitiro <mitrio517@gmail.com>
2026-09-22 16:30:49 -0700

    Create basic HTML structure in style.css
    Added a simple HTML structure with personal information.

---
+++ style.css
@@ -0,0 +1,14 @@
+	<!DOCTYPE html>
+	<html>
+		<head> </head>
+		<body>
+			<img 
+				src=https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRALqcbEwbDJMxu4mg6sEkHs2_dsdrde2hIiGp0Yl6VWg&s=10/>
+			<h1>Hi my name is Demitrio :)</h1>
+			<p1>I was born in Modesto, CA along with my younger sister.</p1>
+			<p2>My favorite hobbies are to play video and board games. Specificaly strategy games, I also like to play tennis.</p2>
+			<p3>I also try to make everyone happy and be kind to everyone,and to be as helpful and caring as I can be since my friends are everything to me :)</p3>
+			<p4>My facts are im 5'4, my favorite colors are red and blue, my favorte baseball team is Giants, 49'ers, I want to live in San Fransico California, Madrid Spain, or Munuch Germany. I dislike conflict and being incompetent</p4>
+		</body>
+		
+	</html>
